50d74019cde772706ceb2d6ba826b2271bfb05da
Emily <emily.tumang@students.olin.edu>
2013-09-23 18:54:23 -0400

    Adding solution to Exercise 3.5

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,31 @@
-"""Solution to an exercise in Think Python.
+#A solution to Exercise 3.5 in Think Python.  Creates a grid of size mxn.
+#Emily Tumang 9/22/13
 
-Author:
-"""
+def grid(numRows, numCols):
+    for i in range (0,numRows):
+        println(numCols)
+        printrow(4,numCols)
+    println(numCols) 
 
-print '+ - - - -'
+def printchar(a,num):
+   for i in range (0,num):
+       print a,
+
+def println(cols):
+    for i in range (0,cols):
+        print '+',
+        printchar('-',4)
+    print '+'
+
+def printrow(height, ncols):
+    for i in range (0,height):
+        for i in range(0,ncols+1):
+            printchar('|',1)
+            printchar(' ',4)
+        print
+    
+grid(4,4)      
+
+
+
+
