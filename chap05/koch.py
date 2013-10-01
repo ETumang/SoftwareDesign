0750f4ad84665b2210d44df65c9a27d800b802b8
Emily <emily.tumang@students.olin.edu>
2013-09-30 21:19:52 -0400

    Added docstrings

--- chap05/koch.py
+++ chap05/koch.py
@@ -1,5 +1,7 @@
 from swampy.TurtleWorld import *
 
+""" Draws a Koch curve of length length.  T is a turtle.  L is a 
+positive numeric. Returns none.""" 
 def koch(T, length):
 	if length<3:
 		fd(T,length)
@@ -13,11 +15,16 @@ def koch(T, length):
 		lt(T,60)
 		koch(T,l)
 
+"""Draws a Koch snowflake with sides of length length. T is a 
+turtle. Length is a positive numeric. Returns none."""
 def snowflake(T,length):
 	for i in range(0,3):
 		koch(T,length)
 		rt(T,120)
 
+"""Draws a Cesaro curve with length length and internal angles
+angle. T is a turtle. Length and angle are positive numerics.
+Returns none."""
 def cesaro(T,length,angle):
 	if length<10:
 		fd(T,length)
