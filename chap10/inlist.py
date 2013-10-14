2b2070d29c80d0b03d4ad458a5297d499412fdf4
Emily <emily.tumang@students.olin.edu>
2013-10-14 01:11:33 -0400

    Modified docstrings.

--- chap10/inlist.py
+++ chap10/inlist.py
@@ -15,6 +15,13 @@ def make_word_list1(f):
 
 
 def is_in_list(L,word):
+'''Finds whether a word is included in a list.
+
+L: list 
+word: string
+
+return: boolean
+'''
     i = bisect_left(L,word)
     if i<len(L):
         if L[i] == word:
@@ -23,17 +30,32 @@ def is_in_list(L,word):
     return False
 
 def is_in_list2(L,word):
+'''Finds whether a word is included in a list.
+
+L: list of strings
+word: string
+
+return: boolean
+'''
     for s in L:
         if s == word:
             return True
         return False
 
 def reverse_pair(L):
+'''Prints a list of words which, when reversed, are also words.
+
+L: list of strings
+'''
     for word in L:
         if is_in_list(L, word[::-1]):
             print word+" "+word[::-1]
 
 def interlock(L):
+'''Prints pairs of words which, when interleaved, form another word.
+
+L: list of strings
+'''
     for word in L:
         
         s1 = ""
@@ -48,6 +70,11 @@ def interlock(L):
             print s1 + " " +s2 + " "+ word
 
 def interlock_many(L,n):
+'''Prints words which can be de-interleaved to form n separate words.
+
+L: list of strings
+n: number of words included in each interleaved word
+'''
     for word in L:
         is_interlock =True
         for i in range(0,n):
